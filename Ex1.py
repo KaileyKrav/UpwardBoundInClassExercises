eda16b4916799a74feeb133815176c32e19c187f
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2022-10-07 15:36:36 -0400

    Create Ex1.py

---
+++ Ex1.py
@@ -0,0 +1,15 @@
+#Kailey Kravabloski
+#9/29/22
+#Ex1
+
+first = input('fname: ')
+last = input('lname: ')
+bdayMonth = input('month: ')
+bdayDay = int(input('day: '))
+bdayYear = int(input('year: '))
+age = 2022 - bdayYear
+
+print('First Name: ', first)
+print('Last Name: ', last)
+print('Birthday: ', bdayMonth, bdayDay, ',', bdayYear)
+print('Age: ', age)
