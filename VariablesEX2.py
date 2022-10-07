5742b3ceecffb86247ea69abb470de35c86dc7c2
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2022-10-07 15:37:36 -0400

    Create VariablesEX2.py

---
+++ VariablesEX2.py
@@ -0,0 +1,17 @@
+#Kailey Kravabloski
+#9/29/22
+#Variables Exercise 2
+
+#Get the user's first name
+first_name = input('Enter your first name: ')
+
+#Get the user's last name
+last_name = input('Enter your last name: ')
+
+#Get the user's age
+age = int(input('Enter your age: '))
+
+#Print out the user's information
+print('First Name: ', first_name)
+print('Last Name: ', last_name)
+print('Age: ', age)
